660ccea9250f1303e769ba2674c45b1f40ddc96d
Matheus Mercadante <zpunchpvp@gmail.com>
2021-11-09 22:38:25 -0300

    feature: initial commit

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,5 +1,5 @@
 region      = "us-central1"
 zone        = "us-central1-c"
-sync_repo   = "https://github.com/terraform-google-modules/terraform-google-kubernetes-engine.git"
-sync_branch = "master"
-policy_dir  = "examples/acm-terraform-blog-part1/config-root"
+sync_repo   = "https://github.com/matheusmercadante/journal-fleet.git"
+sync_branch = "main"
+policy_dir  = "config-root"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,19 +13,23 @@ variable "region" {
 variable "zone" {
   type        = string
   description = "the GCP zone in the region where the cluster will be created"
+  default     = "us-east1-b"
 }
 
 variable "sync_repo" {
   type        = string
   description = "git URL for the repo which will be sync'ed into the cluster via Config Management"
+  default     = "https://github.com/matheusmercadante/journal-fleet.git"
 }
 
 variable "sync_branch" {
   type        = string
   description = "the git branch in the repo to sync"
+  default     = "main"
 }
 
 variable "policy_dir" {
   type        = string
   description = "the root directory in the repo branch that contains the resources."
+  default     = "config-root"
 }
